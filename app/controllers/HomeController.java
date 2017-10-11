536b28b00d92af94946ec67e4b59e31be51a542b
RCI\andrew.killam1 <andrew.killam1>
2017-10-11 15:47:35 -0400

    removed the comments I was testing branches with

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,16 +8,6 @@ import play.mvc.*;
  */
 public class HomeController extends Controller {
 	
-	// adding a comment to LearningBranch01
-	
-	// adding a comment to LearningBranch02
-	
-	// adding another comment to LearningBranch02
-	
-	// adding yet another comment to LearningBranch02
-	
-	// this should only appear in the "alpha" and "beta" branches
-
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
